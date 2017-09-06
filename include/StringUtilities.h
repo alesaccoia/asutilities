3c8aa9666653ceebf07dad9ca11c45548b3e281e
Alessandro Saccoia <alessandro@alsc.co>
2017-09-06 18:38:53 +0200

    String trimming methods

--- include/StringUtilities.h
+++ include/StringUtilities.h
@@ -465,6 +465,45 @@ inline bool numeric_file_compare(const std::string& string1, const std::string& 
     return std::lexicographical_compare(it1, s1.end(), it2, s2.end());
 }
 
+// https://stackoverflow.com/questions/1798112/removing-leading-and-trailing-spaces-from-a-string
+
+// trim from left
+inline std::string& ltrim(std::string& s, const char* t = " \t\n\r\f\v")
+{
+    s.erase(0, s.find_first_not_of(t));
+    return s;
+}
+
+// trim from right
+inline std::string& rtrim(std::string& s, const char* t = " \t\n\r\f\v")
+{
+    s.erase(s.find_last_not_of(t) + 1);
+    return s;
+}
+
+// trim from left & right
+inline std::string& trim(std::string& s, const char* t = " \t\n\r\f\v")
+{
+    return ltrim(rtrim(s, t), t);
+}
+
+// copying versions
+
+inline std::string ltrim_copy(std::string s, const char* t = " \t\n\r\f\v")
+{
+    return ltrim(s, t);
+}
+
+inline std::string rtrim_copy(std::string s, const char* t = " \t\n\r\f\v")
+{
+    return rtrim(s, t);
+}
+
+inline std::string trim_copy(std::string s, const char* t = " \t\n\r\f\v")
+{
+    return trim(s, t);
+}
+
 }}
 
 #endif
