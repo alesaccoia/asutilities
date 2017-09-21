19667d363128305daed84ad71a2ca2d404ab1de8
Alessandro Saccoia <alessandro@alsc.co>
2017-09-21 12:51:34 +0200

    New variance version

--- include/MathUtilities.h
+++ include/MathUtilities.h
@@ -23,26 +23,6 @@ static inline size_t max_index( DataT *const input, const size_t size, float* va
   return max;
 }
 
-/**
- * Computes the mean
- */
-template<class FTYPE>
-inline typename FTYPE::value_type mean1D(const FTYPE& v) {
-  typename FTYPE::value_type sum = std::accumulate(v.begin(), v.end(), 0.0);
-  return sum / v.size();
-}
-
-/**
- * Computes the variance
- */
-template<class FTYPE>
-inline typename FTYPE::value_type variance1D(const FTYPE& v) {
-  auto mean = mean1D(v);
-  typename FTYPE::value_type sq_sum = std::inner_product(v.begin(), v.end(), v.begin(), 0.0);
-  return (sq_sum / v.size() - mean * mean);
-}
-
-
 }
 
 #endif /* MathUtilities_h */
